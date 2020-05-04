a92f3edf7c10af199fae148ba2273ca93a74fb6e
ahmedtofaha10 <ahmedtofaha999@gmail.com>
2020-05-04 02:57:06 +0200

    pages

--- routes/web.php
+++ routes/web.php
@@ -79,8 +79,3 @@ Route::get('/nots', function () {
     return view('site.nots');
 })->name('nots');
 Route::get('/page/{id}','PageController@show')->name('page');
-
-
-Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
-    \UniSharp\LaravelFilemanager\Lfm::routes();
-    });
