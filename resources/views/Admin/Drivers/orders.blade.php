404685e559c9f13a517ccb32d5e1df1e4dd579f0
Muhammad Essam <mohammedessam699@gmail.com>
2020-05-06 00:18:57 +0200

    fixs a bug

--- resources/views/Admin/Drivers/orders.blade.php
+++ resources/views/Admin/Drivers/orders.blade.php
@@ -1,37 +1,43 @@
 @extends('Admin.Layout.layout')
 @section('content')
-    <div class="driver_orders">
-        <h3> طلبات السائق</h3>
-        <h5>{{$orders[0]->driver->user->name}}</h5>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">اسم العميل</th>
-                <th scope="col">الحالة</th>
-                <th scope="col">عدد الاصناف</th>
-                <th scope="col">التاريخ</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($orders as $order)
+    @if($orders->count())
+        <div class="driver_orders">
+            <h3> طلبات السائق</h3>
+            <h5>{{$orders[0]->driver->user->name}}</h5>
+            <table class="table">
+                <thead>
                 <tr>
-                    <th scope="row">{{$order->id}}</th>
-                    <td>{{$order->user->name}}</td>
-                    <td>
-                        @if($order->status == "pending")
-                            معلق
-                        @elseif($order->status == "in_shipping")
-                            جاهز للشحن
-                        @else
-                            لم يحدد بعد
-                        @endif
-                    </td>
-                    <td>{{$order->products->count()}}</td>
-                    <td>{{ $order->created_at->format('y-m-d')}}</td>
+                    <th scope="col">#</th>
+                    <th scope="col">اسم العميل</th>
+                    <th scope="col">الحالة</th>
+                    <th scope="col">عدد الاصناف</th>
+                    <th scope="col">التاريخ</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
+                </thead>
+                <tbody>
+                @foreach($orders as $order)
+                    <tr>
+                        <th scope="row">{{$order->id}}</th>
+                        <td>{{$order->user->name}}</td>
+                        <td>
+                            @if($order->status == "pending")
+                                معلق
+                            @elseif($order->status == "in_shipping")
+                                جاهز للشحن
+                            @else
+                                لم يحدد بعد
+                            @endif
+                        </td>
+                        <td>{{$order->products->count()}}</td>
+                        <td>{{ $order->created_at->format('y-m-d')}}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    @else
+        <h3>
+            لا توجد لدية طلبات
+        </h3>
+    @endif
 @endsection
