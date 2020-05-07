4fa691139050cd2e7da1ca587e6749e4fba67042
Muhammad Essam <mohammedessam699@gmail.com>
2020-05-07 06:44:07 +0200

    some html

--- resources/views/Admin/Layout/nav.blade.php
+++ resources/views/Admin/Layout/nav.blade.php
@@ -70,7 +70,6 @@
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="fa fa-user-circle-o"></i>
-                <span class="badge badge-danger navbar-badge">3</span>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left">
                 <div class="dropdown-divider"></div>
